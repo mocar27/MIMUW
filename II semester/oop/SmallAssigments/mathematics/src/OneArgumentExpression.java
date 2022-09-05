54cfddb3b4e18773d6434bede3c2d2bcd5b60178
Konrad Mocarski <100534891+mocar27@users.noreply.github.com>
2022-09-06 00:15:17 +0200

    Delete II semester/oop/SmallAssigments directory

--- II semester/oop/SmallAssigments/mathematics/src/OneArgumentExpression.java
+++
@@ -1,9 +0,0 @@
-public abstract class OneArgumentExpression extends Expression {
-
-    protected Expression expression;
-
-    public OneArgumentExpression(Expression expression) {
-        this.expression = expression;
-    }
-
-}
